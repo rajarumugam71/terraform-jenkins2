a9a7ef33d8ebb29437f7e7a0210667266050f6b0
unknown <>
2020-11-19 09:18:36 -0500

    terraform2 -11191

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "${var.region}"
 }
+
+terraform {
+
+  backend "s3" {
+
+    bucket = "wsraj-tf-12121118"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
